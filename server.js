3ece1e73faf10df190762fef65001f2c9f17b7fa
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-12-06 16:36:39 +0100

    project reboot

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.search(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// simple route
+app.get('/', (req, res) => {
+    res.json({
+        message:"Salut, bienvenue dans l'API Annuaire"
+    })
+})
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+})
