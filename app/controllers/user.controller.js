3da7ab86f71081c177ea08ae7c9658cfe95c82ba
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-12-08 16:38:39 +0100

    profile route added

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -8,7 +8,15 @@ dotenv.config();
 
 // Create and Save a new User
 exports.register = (req, res) => {
-  User.create(req.body)
+  User.findOrCreate({
+    where: { email: req.body.email },
+    defaults: {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      groupId: req.body.groupId,
+      password: req.body.password
+    }
+  })
     .then(data => {
       res.status(201).send(data);
     })
@@ -20,21 +28,44 @@ exports.register = (req, res) => {
     });
 };
 
-// Get users info with email and password
-exports.login = async(req, res, next) => {
-  const user = await User.findOne({ where : {email : req.body.email }});
-  if(user){
-     const password_valid = await bcrypt.compare(req.body.password,user.password);
-     if(password_valid){
-         token = jwt.sign({ "id" : user.id,"email" : user.email,"first_name":user.first_name },process.env.SECRET);
-         res.status(200).json({ token : token });
-     } else {
-       res.status(400).json({ error : "Password Incorrect" });
-     }
-   
-   }else{
-     res.status(404).json({ error : "User does not exist" });
-   }
+exports.login = async (req, res) => {
+  const user = await User.findOne({ where: { email: req.body.email } });
+
+  if (user) {
+    const password_valid = await bcrypt.compare(req.body.password, user.password);
+    if (password_valid) {
+      token = jwt.sign({ "id": user.id, "email": user.email, "first_name": user.first_name }, process.env.SECRET);
+      res.cookie('token', token, { httpOnly: true });
+      res.status(200).json({ token: token });
+    } else {
+      res.status(400).json({ error: "Password Incorrect" });
+    }
+  } else {
+    res.status(404).json({ error: "User does not exist" });
+  }
+}
+
+exports.profile = async (req, res, next) => {
+  try {
+    let token = req.headers['cookie'].split("=")[1];
+    let decoded = jwt.verify(token, process.env.SECRET);
+    let user = await User.findOne({
+      where: { id: decoded.id },
+      attributes: [
+        "email",
+        "firstname",
+        "lastname",
+        "groupId"
+      ]      
+    });
+    if (user === null) {
+      res.status(404).json({ 'msg': "User not found" });
+    }
+    res.status(200).json(user);
+    next();
+  } catch (err) {
+    res.status(401).json({ "msg": "Couldnt Authenticate" });
+  }
 }
 
 // Retrieve all Users from the database.
@@ -42,9 +73,9 @@ exports.findAll = (req, res) => {
   const email = req.query.email;
   var condition = email ? { email: { [Operator.like]: `%${email}%` } } : null;
 
-  User.findAll({ 
+  User.findAll({
     where: condition,
-    attributes: ['firstname', 'lastname'] 
+    attributes: ['firstname', 'lastname']
   })
     .then(data => {
       res.send(data);
@@ -53,27 +84,6 @@ exports.findAll = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving users."
-      });
-    });
-};
-
-// Find a single User with an id
-exports.findOne = (req, res) => {
-  const id = req.params.id;
-
-  User.findByPk(id)
-    .then(data => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find User with id=${id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: `Error retrieving User with id=${id}.`
       });
     });
 };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,22 +1,21 @@
 module.exports = app => {
-    const users = require("../controllers/user.controller.js");
-  
-    var router = require("express").Router();
-    
-    // Retrieve all Users
-    router.get("/", users.findAll);
-  
-    // Retrieve a single User with id
-    router.get("/:id", users.findOne);
+  const users = require("../controllers/user.controller.js");
 
-    // Update a User with id
-    router.put("/:id", users.update);
-  
-    // Delete a User with id
-    router.delete("/:id", users.delete);
-  
-    // Delete all Users
-    router.delete("/", users.deleteAll);
-  
-    app.use('/api/users', router);
-  };
+  var router = require("express").Router();
+
+  // Retrieve all Users
+  router.get("/", users.findAll);
+
+  router.get("/profile", users.profile)
+
+  // Update a User with id
+  router.put("/:id", users.update);
+
+  // Delete a User with id
+  router.delete("/:id", users.delete);
+
+  // Delete all Users
+  router.delete("/", users.deleteAll);
+
+  app.use('/api/users', router);
+};
